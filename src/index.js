96b40532d268a23e24919b70a812a0eaa3e48aec
sidharth <sidharth.nair@neoito.com>
2023-12-06 14:49:58 +0530

    swagger ui api docs successfully created

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const sequelize = require("./config/database.js");
 const rolesSample = require("./models/rolesSample.js")
-
-const rolesSampleRouter = require("./routes/rolesSample.js");
+const swaggerUi = require("swagger-ui-express");
+const {rolesSampleRouter,swaggerSpec} = require("./routes/rolesSample.js");
 app.use("/v1", rolesSampleRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Serve Swagger UI at /api-docs endpoint
+
 app.listen(3000, () => {
   console.log('Server listening at port 3000');
 rolesSample.sync().then((res)=>{console.log("sample roles table created")})});
--- src/routes/rolesSample.js
+++ src/routes/rolesSample.js
@@ -1,10 +1,28 @@
 const express = require('express')
-const router = express.Router()
+const rolesSampleRouter = express.Router()
 const rolesSampleController = require("../controllers/rolesSample.js");
 const validateInput = require("../middleware/rolesSample.js")
 
+//using swagger dependencies
+const swaggerJSDocs = require("swagger-jsdoc");
+const swaggerOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Student Management API Documentation ",
+        version: "1.0.0",
+        description: "Manage student and department records using postgresql",
+      },
+      servers: [
+        {
+          url: `http://localhost:3000`,
+        },
+      ],
+    },
+    apis: ["./rolesSample.js"], // specify the path to API routes files
+  };
+const swaggerSpec = swaggerJSDocs(swaggerOptions);
 
-router.get('/sample-roles',rolesSampleController.get);
-
-router.post('/sample-roles',validateInput,rolesSampleController.post)
-module.exports = router;
+rolesSampleRouter.get('/sample-roles',rolesSampleController.get);
+rolesSampleRouter.post('/sample-roles',validateInput,rolesSampleController.post)
+module.exports = {rolesSampleRouter,swaggerSpec};
