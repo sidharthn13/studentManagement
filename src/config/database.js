7d51d2ef5eb6ca2ef61a6fc9d481e4fa2618706f
sidharth <sidharth.nair@neoito.com>
2023-12-08 02:35:24 +0530

    fixed errors related to importing

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const  {Sequelize}  = require("sequelize");
 const dotenv = require("dotenv");
 dotenv.config({ path: "/home/sidharth/Git-Commits/studentManagement/.env" });
 const sequelize = new Sequelize(
--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const app = express();
-const sequelize = require("./config/database.js");
+const sequelize = require("./config/database.js")
 const { rolesSampleRouter } = require("./routes/rolesSample.js");
 
 const students = require("./models/students.js")
 const departments = require("./models/departments.js");
 const roles = require("./models/roles.js")
-const scores = require("./models/scores .js")
+const scores = require("./models/scores.js")
 const users = require("./models/users.js")
-
-
 
 app.use("/v1", rolesSampleRouter);
 app.listen(3000, () => {
   console.log("Server listening at port 3000");
-  // students.sync().then(console.log("student table created"));
-  // departments.sync().then(console.log("departments table created"));
+  students.sync().then(console.log("student table created"));
+  departments.sync().then(console.log("departments table created"));
+  roles.sync().then(console.log("roles table created"));
+  scores.sync().then(console.log("scores table created"));
+  users.sync().then(console.log("users table created"));
 
-  sequelize.sync().then(console.log("all tables created"));
+
+  // sequelize.sync().then(console.log("all tables created"));
   // generate_rows()
 });
 
