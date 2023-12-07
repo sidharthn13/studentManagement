56bc058c9572b1256fcd94c8740636438687264a
sidharth <sidharth.nair@neoito.com>
2023-12-07 16:06:39 +0530

    synced all models using sequelize.sync() to make code more readable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const app = express();
-const users = require("./models/users.js")
-const roles = require("./models/roles.js")
-const scores = require("./models/scores.js")
-const students = require("./models/students.js")
-const departments = require("./models/departments.js")
-const courses = require("./models/courses.js")
+const sequelize = require("./config/database.js");
 const { rolesSampleRouter } = require("./routes/rolesSample.js");
 app.use("/v1", rolesSampleRouter);
 app.listen(3000, () => {
-  console.log('Server listening at port 3000');
-  roles.sync({force:true}).then(console.log("roles table created"))
-  users.sync({force:true}).then(console.log("users table created"))
-  scores.sync({force:true}).then(console.log("scores table created"))
-  students.sync({force:true}).then(console.log("students table created"))
-  departments.sync({force:true}).then(console.log("departments table created"))
-  courses.sync({force:true}).then(console.log("courses table created"))
+  console.log("Server listening at port 3000");
+  sequelize.sync({ force: true }).then(console.log("all tables created"));
 });
-
-
--- src/models/courses.js
+++ src/models/courses.js
@@ -1,14 +1,14 @@
-const {DataTypes} = require("sequelize")
-const sequelize = require("../config/database.js")
-const courses = sequelize.define("courses",{
-    id:{
-        type:DataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true
-    },
-    courseName:{
-        type:DataTypes.STRING(20),
-        allowNull:false
-    },
-})
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database.js");
+const courses = sequelize.define("courses", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  courseName: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+  },
+});
 module.exports = courses;
