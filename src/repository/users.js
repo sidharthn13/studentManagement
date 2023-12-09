01a297fc5d2b647702fb0a5a56f021482622763f
sidharth <sidharth.nair@neoito.com>
2023-12-09 21:30:00 +0530

    create user feature implementes successfully

---
+++ src/repository/users.js
@@ -0,0 +1,9 @@
+const users = require("../models/users.js")
+const usersRepository = {
+    createIfNotExists: async function(data){
+            const searchResult = await users.findOrCreate({where:{email:data.email},
+            defaults:data})
+            return searchResult 
+    }
+}
+module.exports = usersRepository
--- src/service/users.js
+++ src/service/users.js
@@ -1,8 +1,15 @@
 const usersRepository = require("../repository/users")
 const usersService = {
-    create:(req,res)=>{
-        console.log(req.body)
-        res.end("user created")
+    create:async(req,res)=>{
+        try{
+        const data = req.body
+        const newUser = await usersRepository.createIfNotExists(data)
+        if(!newUser[1]){return res.status(400).end("user already exists")}
+        return res.status(200).end("user created")}
+        catch(error){
+            res.status(500).json({"Error":error})
+        }
     }
+
 }
 module.exports = usersService
