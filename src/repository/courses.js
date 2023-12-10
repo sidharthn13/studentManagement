8367a04afdc523e7f7a75d0efe3d8d2b3b765963
sidharth <sidharth.nair@neoito.com>
2023-12-10 20:57:21 +0530

    trying to reference the junction table of courses and departments without error

--- src/repository/courses.js
+++ src/repository/courses.js
@@ -4,6 +4,14 @@ const coursesRepository = {
     findDepartment:async function(id){
         result = await departments.findByPk(id)
         return result
+    },
+    createCourse: async function(data){
+        result = await courses.findOrCreate({where:{courseName:data.courseName},
+            defaults:{courseName:data.courseName}})
+        return result;
+    },
+    mapCourseToDepartment: async function(data){
+        
     }
 };
 module.exports = coursesRepository
--- src/service/courses.js
+++ src/service/courses.js
@@ -1,12 +1,20 @@
 const coursesRepository = require("../repository/courses")
 const coursesService ={
     create: async(req,res)=>{
+        try{ 
         const data = req.body
         const department = await coursesRepository.findDepartment(data.departmentID)
         if(!department){
             return res.status(400).json({Error:" No department with that ID exits"})
-        
         }
+        const searchResult = await coursesRepository.createCourse(data)
+        if(!searchResult[1]){return res.status(400).json({Error:"course already exists"})}
+        const newData = {departmentId:data.departmentID,
+                        courseId:searchResult[0].id}
+        // console.log(newData)
+        await coursesRepository.mapCourseToDepartment(newData)
+        return res.status(200).json({Success:"Course added"})}
+        catch(error){return res.status(500).json({Error:error})}
     }
 }
 module.exports = coursesService;
