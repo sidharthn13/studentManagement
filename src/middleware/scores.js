55d9201e2bd9a6cb2ce91846ae57ec15bb76e7ee
sidharth <sidharth.nair@neoito.com>
2023-12-12 13:45:33 +0530

    scores table all features added, edge cases fixed

--- src/middleware/scores.js
+++ src/middleware/scores.js
@@ -12,7 +12,14 @@ const validateScoresData = {
         return true;
       })
       .bail(),
-      body("score").exists().isInt({ min: 0, max:100 }).withMessage("invalid score").toInt()
+      body("score").exists().isInt({ min: 0, max:100 }).withMessage("invalid score").toInt(),
+      (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+      },
     ]
 }
 module.exports = validateScoresData
