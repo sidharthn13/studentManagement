7e978a4d0c5bb9a2160c835a529643fd53f91832
sidharth <sidharth.nair@neoito.com>
2023-12-12 15:57:16 +0530

    made modifications in code to send only necessary data when user requests fro student performance report

--- src/service/performanceReport.js
+++ src/service/performanceReport.js
@@ -3,7 +3,7 @@ const performanceReportService = {
   getReport: async (req, res) => {
     try {
       const result = await performanceReportRepository.getPerformanceReport();
-      return res.status(200).json({ Report: result });
+      return res.status(200).json({ Report: result[0] });
     } catch (error) {
       return res.status(500).json({ Error: error });
     }
