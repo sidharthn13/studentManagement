0640ec6abeca44493518a0b26c8151411f05b89c
sidharth <sidharth.nair@neoito.com>
2023-12-12 11:50:15 +0530

    fixed error handling in score addition feature

--- src/service/scores.js
+++ src/service/scores.js
@@ -1,22 +1,32 @@
-const scoresRepository = require("../repository/scores")
-const departmentsRepository = require("../repository/departments")
-const coursesRepository = require("../repository/courses")
+const scoresRepository = require("../repository/scores");
 const scoresService = {
-    addScore:async(req,res)=>{
-        try{
-            const data = req.body;
-            const studentData = await scoresRepository.findStudentData(data.studentID)
-            const departmentID = studentData.department
-            const courseInDepartment = await scoresRepository.getCourseIfBelongsToDepartment(
-                departmentID,data.courseID);
-            console.log(courseInDepartment)
-            if(!courseInDepartment){return res.status(400).json({Error:"Student is not enrolled in this course"})}
-            const scoreAdded = await scoresRepository.addScore(data)   
-            return res.status(200).json({Success: "Score added to database"}) 
-        }
-        catch(error){
-            return res.status(500).json({Error:error});
-        }
+  addScore: async (req, res) => {
+    try {
+      const data = req.body;
+      const studentData = await scoresRepository.findStudentData(
+        data.studentID
+      );
+      const departmentID = studentData.department;
+      const courseInDepartment =
+        await scoresRepository.getCourseIfBelongsToDepartment(
+          departmentID,
+          data.courseID
+        );
+      if (!courseInDepartment) {
+        return res
+          .status(400)
+          .json({ Error: "Student is not enrolled in this course" });
+      }
+      const scoreAdded = await scoresRepository.addScore(data);
+      if (!scoreAdded[1]) {
+        return res
+          .status(400)
+          .json({ Error: "Student already has a score entry for this course" });
+      }
+      return res.status(200).json({ Success: "Score added to database" });
+    } catch (error) {
+      return res.status(500).json({ Error: error });
     }
+  },
 };
 module.exports = scoresService;
